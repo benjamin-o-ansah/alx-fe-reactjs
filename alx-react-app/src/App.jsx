246bd19d32dcbad03d4b8c523b6c676a4dfdabf5
benoansah2025 <memphisperla@gmail.com>
2025-11-05 07:52:39 +0000

    modified the components declaration with a whitespace

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
@@ -12,10 +12,10 @@ function App() {
 
   return (
     <>
-    <WelcomeMessage/>
-    <Header/>
-    <MainContent/>
-    <Footer/>
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
     </>
     
     // <>
