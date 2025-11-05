98e5edfd517bd25b1ac7508e22d4b989b55f51e8
benoansah2025 <memphisperla@gmail.com>
2025-11-05 07:51:25 +0000

    created 3 components namely Header, main content and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <WelcomeMessage/>
+    <Header/>
+    <MainContent/>
+    <Footer/>
+    </>
+    
     // <>
     // <WelcomeMessage/>
     //   <div>
