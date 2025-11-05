0bd9c753da223d2db3aabb86ca68e7155cc7db0a
benoansah2025 <memphisperla@gmail.com>
2025-11-05 08:17:43 +0000

    made a duplicate of the alx-react-app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor:"darkGreen",color:"white",fontWeight:100}}>
+            <p style={{letterSpacing:"10px"}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
