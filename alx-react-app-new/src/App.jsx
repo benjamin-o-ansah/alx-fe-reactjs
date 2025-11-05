0ce5076e589ea33cc44d281558c2979c1f712647
benoansah2025 <memphisperla@gmail.com>
2025-11-05 08:32:09 +0000

    created a counter function using useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,17 +6,16 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter'
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-    <WelcomeMessage />
+
     <Header />
+    <Counter /> 
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <MainContent />
-    <Footer />
+    <Footer /> 
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
