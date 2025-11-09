6d62134ba6a39e106ee58869da40d3767b94bb23
benoansah2025 <memphisperla@gmail.com>
2025-11-09 12:07:01 +0000

    completed assignment on refactoring of prop drilling to use context

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,17 @@
+import UserContext from './components/userContext';
+import ProfilePage from './components/ProfilePage';
+import './App.css'
+
+function App() {
+ const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+    </>
+  )
+}
+
+export default App
