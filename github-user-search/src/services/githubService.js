9dee3f08dae649654f876be5c445f6dccd17ae7c
benoansah2025 <memphisperla@gmail.com>
2025-11-18 19:51:50 +0000

    modified the project

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
   // Construct GitHub Search API query
@@ -8,7 +8,7 @@ export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
   if (location) query += ` location:${location}`;
   if (minRepos) query += ` repos:>=${minRepos}`;
 
-  const url = `${BASE_URL}/search/users?q=${encodeURIComponent(query)}`;
+  const url = `${BASE_URL}${encodeURIComponent(query)}`;
 
   try {
     const response = await axios.get(url);
