1da4a63f75dffe0bd0d89f7da9dd68e50fba1b67
benoansah2025 <memphisperla@gmail.com>
2025-11-18 19:16:37 +0000

    updated the name to Search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Search from "./components/SearchForm"
+import Search from "./components/Search"
 
 function App() {
   const [count, setCount] = useState(0)
