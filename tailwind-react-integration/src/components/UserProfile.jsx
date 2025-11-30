09b02c326fc306ea195900baf57aefc2049f1094
memphisperla <memphisperla@gmail.com>
2025-11-30 13:03:54 +0000

    reverted styles to its previous mdoe

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 max-w-xs md:max-w-sm p-8">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:max-w-sm md:p-8">
       <img src="https://via.placeholder.com/150" alt="User" className="sm: w-24 h-24 md:w-36 h-36 rounded-full w-36 h-36 mx-auto"/>
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
