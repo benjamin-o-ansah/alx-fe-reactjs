0212852927127397545b744b0acd88ecf4e8231a
benoansah2025 <memphisperla@gmail.com>
2025-12-17 20:12:26 +0000

    added import todolist

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import TodoPage from './components/TodoPage';
+import TodoList from './components/TodoList';
 function App() {
   // const [count, setCount] = useState(0)
   const [todos, setTodos] = useState([]);
